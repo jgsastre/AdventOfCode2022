70d79cff332c96199f67ded7e16756b44bc2720d
Joaquin Miguel Garcia Sastre <jgarciasastre@twilio.com>
2022-12-20 09:23:39 +0100

    Finished day5

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,9 +1,7 @@
-use std::{
-    collections::HashMap,
-    str::{FromStr, Lines},
-};
+use std::{collections::HashMap, str::FromStr};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use itertools::Itertools;
 //use itertools::Itertools;
 
 #[derive(Debug)]
@@ -12,7 +10,26 @@ struct Crane {
 }
 
 impl Crane {
-    fn apply_move(&mut self, movement: &Move) -> &Self {
+    fn move_one_by_one(&mut self, movement: &Move) -> &Self {
+        let _removed_ements: Vec<_> = (0..movement.quantity)
+            .map(|_| {
+                let item = self
+                    .stack
+                    .get_mut(&movement.origin)
+                    .expect("Origin must exists")
+                    .pop()
+                    .unwrap();
+                self.stack
+                    .get_mut(&movement.destiny)
+                    .expect("Destiny must exists")
+                    .push(item);
+            })
+            .collect();
+
+        self
+    }
+
+    fn move_in_bulk(&mut self, movement: &Move) -> &Self {
         let removed_ements = (0..movement.quantity)
             .map(|_| {
                 self.stack
@@ -21,10 +38,11 @@ impl Crane {
                     .pop()
                     .unwrap()
             })
-            .rev()
             .collect::<Vec<_>>();
+
         removed_ements
             .into_iter()
+            .rev()
             .for_each(|x| self.stack.get_mut(&movement.destiny).unwrap().push(x));
 
         self
@@ -38,12 +56,10 @@ impl FromStr for Crane {
         let mut stack = HashMap::<u8, Vec<char>>::new();
 
         for line in s.lines().rev().skip(1) {
-            println!("Parsing line: {line}");
             let mut idx: u8 = 1;
             let mut i = 0;
             while i < line.len() {
                 let token = &line[i..(i + 3)];
-                println!("Parsing \"{token}\"");
                 if !token.trim().is_empty() {
                     let value = token[1..].chars().next().expect("Empty token!");
                     stack
@@ -60,26 +76,90 @@ impl FromStr for Crane {
     }
 }
 
+#[derive(Debug)]
 struct Move {
     quantity: usize,
     origin: u8,
     destiny: u8,
 }
 
+impl FromStr for Move {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self> {
+        match s.split(" ").collect::<Vec<_>>()[..] {
+            [_, q, _, o, _, d] => Ok(Self {
+                quantity: q.parse()?,
+                origin: o.parse()?,
+                destiny: d.parse()?,
+            }),
+            _ => Err(anyhow!("Imposible parse {s}")),
+        }
+    }
+}
+
 fn main() -> Result<()> {
+
     let solution_1 = solve_part_one();
 
     println!("The solution of part one is {:?}", solution_1);
 
+    let solution_2 = solve_part_two();
+
+    println!("The solution of part two is {:?}", solution_2);
+
     return Ok(());
 }
 
-fn solve_part_one() -> Result<usize> {
-    let _it = include_str!("./day5.test").lines();
+fn solve_part_one() -> Result<String> {
+    let (crane, movements) = include_str!("./day5.input").split_once("\n\n").unwrap();
+    let mut initial_crane: Crane = crane.parse()?;
+    movements
+        .lines()
+        .map(|line| line.parse::<Move>().unwrap())
+        .for_each(|ref m| {
+            initial_crane.move_one_by_one(m);
+        });
 
-    return Ok(5);
+    let mut result = String::new();
+    initial_crane
+        .stack
+        .into_iter()
+        .sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
+        .for_each(|(_, v)| {
+            if let Some(&c) = v.last() {
+                result.push(c);
+            }
+        });
+
+    return Ok(result);
 }
 
+fn solve_part_two() -> Result<String> {
+    let (crane, movements) = include_str!("./day5.input").split_once("\n\n").unwrap();
+    let mut initial_crane: Crane = crane.parse()?;
+    movements
+        .lines()
+        .map(|line| line.parse::<Move>().unwrap())
+        .for_each(|ref m| {
+            initial_crane.move_in_bulk(m);
+        });
+
+    let mut result = String::new();
+    initial_crane
+        .stack
+        .into_iter()
+        .sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
+        .for_each(|(_, v)| {
+            if let Some(&c) = v.last() {
+                result.push(c);
+            }
+        });
+
+    return Ok(result);
+}
+
+#[cfg(test)]
 #[cfg(test)]
 mod tests {
 
